ebfad0c2f5ef966056e653c48a3820d2d64916a0
RyanMoodGAMING <ryanmood.098@gmail.com>
2021-02-03 14:58:39 +0000

    Updated it to the snakify model answer.

--- 10 - Lost Card.py
+++ 10 - Lost Card.py
@@ -1,5 +1,10 @@
 n = int(input())
-s = (n + 1) * n / 2
-for i in range(n-1):
-    s -= int(input())
-print(int(s))
+sum_cards = 0
+for i in range(1, n + 1):
+    sum_cards += i
+# One can prove the following:
+# sum_cards == n * (n + 1) // 2
+# However, we'll calculate that using the loop.
+for i in range(n - 1):
+    sum_cards -= int(input())
+print(sum_cards)
